502f02f15b595fb56f585e33eb8e4eb513107d6c
doublespiral <113387397+doublespiral@users.noreply.github.com>
2024-10-28 17:12:07 -0400

    Delete old directory
    No longer required after refactor

--- examples/lol/tiny.nim
+++
@@ -1,7 +0,0 @@
-# /*
-#[*/
-int add(int a,int b){return a+b;}
-# /*]##*/
-#ifdef n
-{.passL:"-L. -l:examples/lol/module.o".};proc add(a,b:cint):cint{.importc:"add".};echo add(210,210)
-#endif
--- examples/simple/main.nim
+++
@@ -1,9 +0,0 @@
-import os
-
-const cmodule = "examples"/"simple"/"csource"/"module.o"
-{.passL: "-L. -l:" & cmodule.}
-
-proc add(a, b: cint): cint {.importc: "add".}
-
-when is_main_module:
-    echo add(210, 210)
